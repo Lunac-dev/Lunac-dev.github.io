5ff4bef97e2bfb36ab61b140a53fe9fb30783122
LunaCC-cloud <61586841+LunaCC-cloud@users.noreply.github.com>
2021-04-11 14:24:43 +0900

    commit

--- Glance/js/js.js
+++ Glance/js/js.js
@@ -5,3 +5,12 @@ function copy() {
     document.execCommand("copy");
     alert("Copied the ip!");
 }
+
+function play() {
+    const p1 = document.getElementById("play");
+    var music = new Audio('images/glancebg.mp3');
+    var v = document.getElementById('video');
+    music.play();
+    v.play();
+    p1.style.display ="none";
+}
